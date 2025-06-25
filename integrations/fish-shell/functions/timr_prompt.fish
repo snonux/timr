bfd44998d3ab14e5f240dad6a52dd89a94659fbc
Paul Buetow <paul@buetow.org>
2025-06-26 00:10:17 +0300

    fix

--- integrations/fish-shell/functions/timr_prompt.fish
+++ integrations/fish-shell/functions/timr_prompt.fish
@@ -1,9 +1,9 @@
-function timr_prompt -d "Display timr status in the prompt"
+function timr_prompt -d "Display timr timr_status in the prompt"
     if command -v timr >/dev/null
-        set -l status (timr prompt)
-        if test -n "$status"
-            set -l icon (string sub -l 1 -- "$status")
-            set -l time (string sub -s 2 -- "$status")
+        set -l timr_status (timr prompt)
+        if test -n "$timr_status"
+            set -l icon (string sub -l 1 -- "$timr_status")
+            set -l time (string sub -s 2 -- "$timr_status")
             if test "$icon" = "▶"
                 set_color green
             else
